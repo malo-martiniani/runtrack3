9477d2a4ed457463aad7f6f702c1c5eb3d9ed96a
Malo Martiniani <malo.martiniani@laplateforme.io>
2025-12-16 15:04:44 +0100

    Add initial HTML structure and Bootstrap grid layout to index.php

---
+++ jour06/job03/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grid</title>
+    <link href="./assets/bootstrap-5.3.8/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="m-0">
+    <div class="container-fluid p-2">
+     
+        <div class="row g-2 mb-2">
+            <div class="col-12">
+                <div class="bg-info" style="height: 80px;"></div>
+            </div>
+        </div>
+
+        <div class="row g-2 mb-2">
+            <div class="col-12 col-md-4">
+                <div class="bg-primary" style="height: 80px;"></div>
+            </div>
+            <div class="col-6 col-md-4">
+                <div class="bg-primary" style="height: 80px;"></div>
+            </div>
+            <div class="col-6 col-md-4">
+                <div class="bg-primary" style="height: 80px;"></div>
+            </div>
+        </div>
+
+        <div class="row g-2 mb-2">
+            <div class="col-12 col-md-4">
+                <div class="bg-primary opacity-50" style="height: 80px;"></div>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="bg-primary opacity-50" style="height: 80px;"></div>
+            </div>
+            <div class="col-md-4 d-none d-md-block">
+                <div class="bg-primary opacity-50" style="height: 80px;"></div>
+            </div>
+        </div>
+
+        <div class="row g-2 mb-2 justify-content-center d-md-none">
+            <div class="col-4">
+                <div class="bg-primary opacity-25" style="height: 40px;"></div>
+            </div>
+        </div>
+
+        <div class="row g-2 mb-2 justify-content-center d-md-none">
+            <div class="col-3">
+                <div style="height: 30px; background-color: #b388ff;"></div>
+            </div>
+        </div>
+
+
+        <div class="row g-2 mb-2">
+            
+            <div class="col-5 mx-auto d-md-none">
+                <div style="height: 40px; background-color: #b388ff;"></div>
+            </div>
+           
+            <div class="col-md-8 d-none d-md-block">
+                <div style="height: 80px; background-color: #b388ff;"></div>
+            </div>
+            <div class="col-md-4 d-none d-md-block">
+                <div style="height: 80px; background-color: #b388ff;"></div>
+            </div>
+        </div>
+
+        <div class="row g-2">
+            <div class="col-4 col-md-2">
+                <div style="height: 50px; background-color: #d39c7a;"></div>
+            </div>
+            <div class="col-4 col-md-2 offset-md-3">
+                <div style="height: 50px; background-color: #d39c7a;"></div>
+            </div>
+            <div class="col-4 col-md-2 offset-md-3">
+                <div style="height: 50px; background-color: #d39c7a;"></div>
+            </div>
+        </div>
+    </div>
+    <script src="./assets/bootstrap-5.3.8/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
