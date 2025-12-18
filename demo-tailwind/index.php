675a0515a9af560eebfb7b04d628b992e5b5dd74
Malo Martiniani <malo.martiniani@laplateforme.io>
2025-12-18 12:04:46 +0100

    chore: add Tailwind CSS setup with CLI and scripts for development and production

---
+++ demo-tailwind/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/app.css">
+</head>
+<body>
+    <h1 class="text-3xl font-bold underline text-center text-teal-400">
+        Hello, Tailwind CSS!
+    </h1>
+</body>
+</html>
